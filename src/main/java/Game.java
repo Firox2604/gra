859137b9e76c7bcde70bb56a650ec267af1ae44a
Pawel.Sleszynski <sleszynski.16@hotmail.com>
2018-05-13 15:48:31 +0200

    przed rafaktoryzacja

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,37 +1,89 @@
 public class Game {
     static final Character EMPTY = ' ';
     private Character[][] gameBoard = {
-            {EMPTY,EMPTY,EMPTY},
-            {EMPTY,EMPTY,EMPTY},
-            {EMPTY,EMPTY,EMPTY}
+            {EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY}
     };
-    private void isValidPlace(int position)
-    {
-        if(position  < 1 || position > 3)
-        {
+
+    private Character nextPLayer = 'X';
+
+    private String winner = "NN";
+
+    private void isValidPlace(int position) {
+        if (position < 1 || position > 3) {
             throw new RuntimeException();
         }
     }
 
-    public void placeMark(int c, int r){
+    public void placeMark(int c, int r) {
         isValidPlace(c);
         isValidPlace(r);
-        if(isAvailablePlace(c,r))
-        {
-            gameBoard[c - 1][r - 1] = 'X';
+        if (isAvailablePlace(c, r)) {
+            gameBoard[c - 1][r - 1] = nextPLayer;
         }
+
+        isTheWinnerKnown();
+        changeNextPlayer();
+    }
+
+    private void isTheWinnerKnown() {
+        for (int iterator = 1; iterator < 4; iterator++) {
+            if (getMarkSightAt(1, iterator)
+                    + getMarkSightAt(2, iterator)
+                    + getMarkSightAt(3, iterator)
+                    == nextPLayer * 3) {
+                winner = String.valueOf(nextPLayer);
+                return;
+            }
+
+            if (getMarkSightAt(iterator, 1)
+                    + getMarkSightAt(iterator, 2)
+                    + getMarkSightAt(iterator, 3)
+                    == nextPLayer * 3) {
+                winner = String.valueOf(nextPLayer);
+                return;
+            }
+        }
+
+        if (getMarkSightAt(1, 1)
+                + getMarkSightAt(2, 2)
+                + getMarkSightAt(3, 3)
+                == nextPLayer * 3) {
+            winner = String.valueOf(nextPLayer);
+            return;
+        }
+
+        if (getMarkSightAt(3, 1)
+                + getMarkSightAt(2, 2)
+                + getMarkSightAt(1, 3)
+                == nextPLayer * 3) {
+            winner = String.valueOf(nextPLayer);
+            return;
+        }
+    }
+
+    private void changeNextPlayer() {
+        nextPLayer = (nextPLayer == 'X') ? 'O' : 'X';
     }
 
     private boolean isAvailablePlace(int c, int r) {
         Character field = gameBoard[c - 1][r - 1];
-        if(field != EMPTY)
-        {
+        if (field != EMPTY) {
             throw new RuntimeException("Field already occupied");
         }
         return true;
     }
 
     public Character getNextPlayer() {
-        return 'X';
+        return nextPLayer;
+    }
+
+    public Character getMarkSightAt(int c, int r) {
+        return gameBoard[c - 1][r - 1];
+    }
+
+    public String andTheWinnerIs() {
+        return "The winner is " + winner;
     }
 }
--- src/test/java/GameTests.java
+++ src/test/java/GameTests.java
@@ -2,7 +2,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertNotNull;
+
+//import static org.junit.Assert.*;
+
 
 public class GameTests {
     private  Game game;
@@ -31,8 +35,77 @@ public class GameTests {
     }
 
     @Test
-    public void whenIsNewGameNextPlyerIsX() {
+    public void whenIsNewGameNextPlayerIsX() {
         Character player = game.getNextPlayer();
         assertThat('X', is(equalTo(player)));
+    }
+
+    @Test
+    public void secondMoveIsForPlayer() {
+        game.placeMark(1,1);
+        Character player = game.getNextPlayer();
+        assertThat('O', is(equalTo(player)));
+    }
+
+    @Test
+    public void secondMoveIsForPlayerOAndMarkIsO() {
+        game.placeMark(1,1);
+        game.placeMark(2,2);
+        Character xSign = game.getMarkSightAt(1,1);
+        Character oSign = game.getMarkSightAt(2,2);
+        assertThat('X', is(equalTo(xSign)));
+        assertThat('O', is(equalTo(oSign)));
+    }
+
+    @Test
+    public void threeSignInRowAtRowWin() {
+//        game.placeMark(1,1);
+//        game.placeMark(2,3);
+//        game.placeMark(2,1);
+//        game.placeMark(2,2);
+//        game.placeMark(3,1);
+        game.placeMark(1,2);
+        game.placeMark(1,1);
+        game.placeMark(2,2);
+        game.placeMark(3,1);
+        game.placeMark(3,2);
+        String winner = game.andTheWinnerIs();
+        assertThat(winner, is(equalTo("The winner is X")));
+    }
+
+    @Test
+    public void threeSignInRowAtColumnWin() {
+        game.placeMark(1,1);
+        game.placeMark(3,1);
+        game.placeMark(1,2);
+        game.placeMark(3,2);
+        game.placeMark(2,2);
+        game.placeMark(3,3);
+        String winner = game.andTheWinnerIs();
+        assertThat(winner, is(equalTo("The winner is O")));
+    }
+
+    @Test
+    public void threeSignInRowAtDiagonalFromLeftToRightWin() {
+        game.placeMark(2,1);
+        game.placeMark(1,1);
+        game.placeMark(3,1);
+        game.placeMark(2,2);
+        game.placeMark(1,3);
+        game.placeMark(3,3);
+        String winner = game.andTheWinnerIs();
+        assertThat(winner, is(equalTo("The winner is O")));
+    }
+
+    @Test
+    public void threeSignInRowAtDiagonalFromRightToLeftWin() {
+        game.placeMark(2,1);//x
+        game.placeMark(3,1);//o
+        game.placeMark(3,2);//x
+        game.placeMark(2,2);//o
+        game.placeMark(2,3);//x
+        game.placeMark(1,3);//o
+        String winner = game.andTheWinnerIs();
+        assertThat(winner, is(equalTo("The winner is O")));
     }
 }
