1dd7887625f4410b3bd5ee4e03d96843e86ad453
Pawel.Sleszynski <sleszynski.16@hotmail.com>
2018-05-13 14:15:49 +0200

    second

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,33 @@
 public class Game {
-    public Game() {
+    static final Character EMPTY = ' ';
+    private Character[][] gameBoard = {
+            {EMPTY,EMPTY,EMPTY},
+            {EMPTY,EMPTY,EMPTY},
+            {EMPTY,EMPTY,EMPTY}
+    };
+    private void isValidPlace(int position)
+    {
+        if(position  < 1 || position > 3)
+        {
+            throw new RuntimeException();
+        }
+    }
+
+    public void placeMark(int c, int r){
+        isValidPlace(c);
+        isValidPlace(r);
+        if(isAvailablePlace(c,r))
+        {
+            gameBoard[c - 1][r - 1] = 'X';
+        }
+    }
+
+    private boolean isAvailablePlace(int c, int r) {
+        Character field = gameBoard[c - 1][r - 1];
+        if(field != EMPTY)
+        {
+            throw new RuntimeException("Field already occupied");
+        }
+        return true;
     }
 }
--- src/test/java/GameTests.java
+++ src/test/java/GameTests.java
@@ -1,12 +1,31 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class GameTests {
+    private  Game game;
 
+    @Before
+    public void setUp()
+    {
+        game = new Game();
+    }
     @Test
     public void gameExist() {
-        Game game = new Game();
-        assertNoNull(game);
+        assertNotNull(game);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void whenMoveOuterBoundThenException() {
+        game.placeMark(5, -1);
+        game.placeMark(-2, 4);
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void whenFieldIsOccupiedTheException() {
+        game.placeMark(2,2);
+        game.placeMark(2,2);
     }
 }
